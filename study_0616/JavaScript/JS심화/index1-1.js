3f0fd744af7a000a0a0ca24e87eea48a12ad56ff
j-i-hyeon <jihyeon020810@gmail.com>
2022-06-15 04:45:19 +0900

    delete folder

--- study_0616/JavaScript/JS심화/index1-1.js
+++
@@ -1,3 +0,0 @@
-const input = prompt();
-
-console.log(input || "입력값이 없습니다.");
--- study_0616/JavaScript/JS심화/스프레드배열.js
+++
@@ -1,4 +0,0 @@
-const animals = ["개", "고양이"];
-const anotherAnimals = [...animals, "참새"];
-
-console.log(anotherAnimals);
