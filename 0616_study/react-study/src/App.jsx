452ff6611d0ded6b0feaa794d3ec0078992500df
j-i-hyeon <jihyeon020810@gmail.com>
2022-06-15 23:29:17 +0900

    react2

--- 0616_study/react-study/src/App.jsx
+++ 0616_study/react-study/src/App.jsx
@@ -1,104 +1,46 @@
-import { Main, MediaDiv } from './styledComponent';
+import { Main, MediaDiv, FooterBig, FooterSmall } from './styledComponent';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, GlobalStyles, lightTheme } from './styles';
-
-// yarn add @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome @fortawesome/fontawesome-svg-core @fortawesome/free-brands-svg-icons
-import {
-    faSun,
-    faMoon,
-    faArrowsRotate,
-    faPenToSquare,
-    faLocationPin,
-    faArrowLeft,
-    faArrowRight,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact } from '@fortawesome/free-brands-svg-icons';
-
-
 import { useState } from 'react';
 import Header from './Header';
 import Slogun from './Slogun';
 import ShowPostList from './ShowPostList';
 import Footer from './Footer';
 
-
 function App() {
+    const initialPostList = [
+        { id: 1, title: '학보, 시사N 대학기자상 취재', replCount: 1 },
+        { id: 2, title: '학보, 시사N 대학기자상 취재', replCount: 43 },
+        { id: 3, title: '학보, 시사N 대학기자상 취재', replCount: 2 },
+    ];
+    
     const [darkMode, setDarkMode] = useState(true);
-    const loading = false;
-    const isPost = true;
+    const [loading, setLoading] = useState(false);
+    const [isPost, setIsPost] = useState(false);
+    const [postList, setPostList] = useState(initialPostList);
+    
+    const addPost = () => {
+        setPostList((postList) => [
+            ...postList,
+            { id: 4, title: '학보, 시사N 대학기자상 취재', replCount: 21 },
+        ]);
+      };
     return (
         <>
             <ThemeProvider theme={darkMode ?darkTheme :lightTheme}>
                 <GlobalStyles/>
                 <MediaDiv>
-                    <Header darkMode={darkMode} setDarkMode={setDarkMode}>
-                        <TitleLogoDiv>
-                            <TitleBig>멋사</TitleBig>
-                            <TitleSmall>익명게시판</TitleSmall>
-                        </TitleLogoDiv>
-                        <SubHeaderDiv>
-                            {darkMode ? (
-                                <div>
-                                    <FontAwesomeIcon icon={faSun}/>
-                                </div>
-                            ) : (
-                                <div>
-                                    <FontAwesomeIcon icon={faMoon}/>
-                                </div>
-                            )}
-                        </SubHeaderDiv>
-                    </Header>
+                    <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
                     <Main>
-                        <SlogunSection>
-                            <SlogunBig>HACK YOUR LIFE</SlogunBig>
-                            <SlogunSmall>
-                                내 아이디어를 내 손으로 실현한다.
-                            </SlogunSmall>
-                        </SlogunSection>
-                        <PostSection>
-                            <PostTitleDiv>
-                                <FontAwesomeIcon icon={faArrowsRotate}/>
-                                <PostTitle>익명게시판</PostTitle>
-                                <FontAwesomeIcon icon={faPenToSquare}/>
-                            </PostTitleDiv>
-                            <PostListDiv>
-                                {loading ? (
-                                    <LoadingDiv>
-                                        <LoadingImg src={loadingIcon}/>
-                                    </LoadingDiv>
-                                ) : isPost ? (
-                                    <LoadingDiv>아직 기록된 글이 없습니다.</LoadingDiv>
-                                ) : (
-                                    <ul>
-                                        <EachPostLi>
-                                            <div>
-                                                <FontAwesomeIcon icon={faLocationPin}/>
-                                                <PostLink>
-                                                    서강학보, 시사 N 대학기자상 취재
-                                                </PostLink>
-                                            </div>
-                                            <PostRepl>[35]</PostRepl>
-                                        </EachPostLi>
-                                    </ul>
-                                )}
-                            </PostListDiv>
-                        </PostSection>
-                        <PagingSection>
-                            <PagenumberDiv>
-                                <FontAwesomeIcon icon={faArrowLeft}/>
-                            </PagenumberDiv>
-                            <PagenumberDiv>2</PagenumberDiv>
-                            <PagenumberDiv>
-                                <FontAwesomeIcon icon={faArrowRight}/>
-                            </PagenumberDiv>
-                        </PagingSection>
+                        <Slogun></Slogun>
+                        <ShowPostList
+                            loading={loading}
+                            isPost={isPost}
+                            postList={postList}
+                            addPost={addPost}
+                        ></ShowPostList>
                     </Main>
-                    <Footer>
-                        <FontAwesomeIcon icon={faReact}/>
-                        <FooterBig>for react study</FooterBig>
-                        <FooterSmall>2022. by jihyeon</FooterSmall>
-                    </Footer>
+                    <Footer></Footer>
                 </MediaDiv>
             </ThemeProvider>
         </>
--- 0616_study/react-study/src/Header.jsx
+++ 0616_study/react-study/src/Header.jsx
@@ -10,24 +10,16 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
-import { useNavigate } from 'react-router-dom';
 function Header({ darkMode, setDarkMode }) {
-  const navigate = useNavigate();
-
-  const goHome = () => {
-    navigate('/');
-  };
-  const toggleDarkMode = () => {
+    const toggleDarkMode = () => {
     setDarkMode((darkMode) => !darkMode);
   };
   return (
     <HeaderDiv>
-      <CursorDiv>
-        <TitleLogoDiv onClick={goHome}>
+        <TitleLogoDiv>
           <TitleBig>멋사</TitleBig>
           <TitleSmall>익명게시판</TitleSmall>
         </TitleLogoDiv>
-      </CursorDiv>
       <SubHeaderDiv>
         {darkMode ? (
           <FontAwesomeIcon onClick={toggleDarkMode} icon={faSun} />
